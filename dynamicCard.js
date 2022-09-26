5362e3d114747b039b70b477688c90573c619cfc
jerzka <kjerzmanowska@gmail.com>
2022-09-26 00:31:52 -0500

    layout updates

--- dynamicCard.js
+++ dynamicCard.js
@@ -5,10 +5,10 @@ if ('content' in document.createElement('template')) {
     console.log("tak");
 }
 const product = (prodName, prodPhoto, prodDescription) => ({
-    cardTitle: `Coffee ${prodName}`,
+    cardTitle: prodName,
     cardImage: prodPhoto,
     cardDesc: prodDescription,
-    cardPrice: (Math.random()*100).toFixed(2),
+    cardPrice: (Math.random()*10).toFixed(2),
     get quantity() {
         return this._quantity;
     },
@@ -18,16 +18,17 @@ const product = (prodName, prodPhoto, prodDescription) => ({
     sellOne: function(evt){
         console.log("one sold");
         let amount = + evt.target.innerText.split(" ")[3];
+        console.log(this);
         console.log("this._quantity " + this._quantity);
 
         if(amount > 0){
-            console.log(typeof amount, amount-1)
-            this._quantity = amount - 1; 
+            this._quantity = + amount - 1; 
             evt.target.textContent = `Buy one of ${this._quantity}`;  
             }
         else{
             alert("Sorry, product is not available.");
         }
+        console.log("this", this);
         console.log(`prod quantity is ${this._quantity}`);
     },
     propagate: function(){
@@ -41,7 +42,7 @@ const product = (prodName, prodPhoto, prodDescription) => ({
         cardDescClone = clone.querySelector("#cardDesc");
         cardDescClone.textContent = this.cardDesc;
         cardPriceClone = clone.querySelector("#cardPrice");
-        cardPriceClone.textContent = this.cardPrice;
+        cardPriceClone.textContent = `${this.cardPrice} CAD`;
         cardOrderBtnClone = clone.querySelector("#cardOrderBtn");
         cardOrderBtnClone.textContent = `Buy one of ${this._quantity}`;
         cardOrderBtnClone.addEventListener('click', this.sellOne);
@@ -51,8 +52,8 @@ const product = (prodName, prodPhoto, prodDescription) => ({
 
 
 const productsArray = [
-    product("Cafe Americano", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-03/3-CaffeAmericano_ContactShadow_Green.png?itok=Kl-bJG_e", "In the morning"),
-    product("Cafe au Lait", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-05/10032021_BAB_CAFE_AU_LAIT_CS-min.png?itok=JoWLDPcf", "In the afternoon"),
+    product("Café Americano", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-03/3-CaffeAmericano_ContactShadow_Green.png?itok=Kl-bJG_e", "In the morning"),
+    product("Café au Lait", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-05/10032021_BAB_CAFE_AU_LAIT_CS-min.png?itok=JoWLDPcf", "In the afternoon"),
     product("Capuccino", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-05/10032021_CAPPUCCINO_CS-min.png?itok=n_45xlrE", "In the evening")
 ];
 
