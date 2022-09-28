c94464f49b2b689f82261128482615b61e4e6d29
jerzka <kjerzmanowska@gmail.com>
2022-09-27 23:43:03 -0500

    adding sellOne binding and button disabled class, rewriting quantity condition, deleting console logs

--- dynamicCard.js
+++ dynamicCard.js
@@ -1,61 +1,62 @@
-const container = document.getElementById("productsContainer");
-const template = document.getElementsByTagName("template")[0];  
-
 if ('content' in document.createElement('template')) {
-    console.log("tak");
-}
-const product = (prodName, prodPhoto, prodDescription) => ({
-    cardTitle: prodName,
-    cardImage: prodPhoto,
-    cardDesc: prodDescription,
-    cardPrice: (Math.random()*10).toFixed(2),
-    get quantity() {
-        return this._quantity;
-    },
-    set quantity(value){
-        this._quantity = value;
-    },
-    sellOne: function(evt){
-        console.log("one sold");
-        let amount = + evt.target.innerText.split(" ")[3];
-        console.log(this);
-        console.log("this._quantity " + this._quantity);
+    console.log("Your browser supports the HTML template element.");
 
-        if(amount > 0){
-            this._quantity = + amount - 1; 
-            evt.target.textContent = `Buy one of ${this._quantity}`;  
-            }
-        else{
-            alert("Sorry, product is not available.");
+    const container = document.getElementById("productsContainer");
+    const template = document.getElementsByTagName("template")[0];  
+
+    const productConstructor = (prodName, prodPhoto, prodDescription) => {
+        return {
+            cardTitle: prodName,
+            cardImage: prodPhoto,
+            cardDesc: prodDescription,
+            cardPrice: (Math.random()*10).toFixed(2),
+            get quantity() {
+                return this._quantity;
+            },
+            set quantity(value){
+                this._quantity = value;
+            },
+            sellOne: function(evt){
+                console.log("one sold");
+              
+                if(this.quantity > 0){
+                    this.quantity -= 1; 
+                    evt.target.textContent = `Buy one of ${this.quantity}`;  
+                    }
+                else{
+                    evt.target.classList.add('disabled');
+                    alert("Sorry, product is not available.");
+                }
+            },
+            populate: function() {
+                this.quantity = Number((Math.random()*10).toFixed());
+                const clone = template.content.cloneNode(true);
+                cardImgClone = clone.querySelector("#cardImg");
+                cardImgClone.src = this.cardImage;
+                cardDiv = clone.querySelector("div");
+                cardTitleClone = cardDiv.querySelector("#cardTitle");
+                cardTitleClone.textContent = this.cardTitle;
+                cardDescClone = clone.querySelector("#cardDesc");
+                cardDescClone.textContent = this.cardDesc;
+                cardPriceClone = clone.querySelector("#cardPrice");
+                cardPriceClone.textContent = `${this.cardPrice} CAD`;
+                cardOrderBtnClone = clone.querySelector("#cardOrderBtn");
+                cardOrderBtnClone.textContent = `Buy one of ${this.quantity}`;
+                this.sellOne = this.sellOne.bind(this);
+                cardOrderBtnClone.addEventListener('click', this.sellOne);
+                container.appendChild(clone);
+            },
         }
-        console.log("this", this);
-        console.log(`prod quantity is ${this._quantity}`);
-    },
-    propagate: function(){
-        this._quantity = Number((Math.random()*10).toFixed());
-        const clone = template.content.cloneNode(true);
-        cardImgClone = clone.querySelector("#cardImg");
-        cardImgClone.src = this.cardImage;
-        cardDiv = clone.querySelector("div");
-        cardTitleClone = cardDiv.querySelector("#cardTitle");
-        cardTitleClone.textContent = this.cardTitle;
-        cardDescClone = clone.querySelector("#cardDesc");
-        cardDescClone.textContent = this.cardDesc;
-        cardPriceClone = clone.querySelector("#cardPrice");
-        cardPriceClone.textContent = `${this.cardPrice} CAD`;
-        cardOrderBtnClone = clone.querySelector("#cardOrderBtn");
-        cardOrderBtnClone.textContent = `Buy one of ${this._quantity}`;
-        cardOrderBtnClone.addEventListener('click', this.sellOne);
-        container.appendChild(clone);
-    }
-});
+    };
 
-
-const productsArray = [
-    product("Café Americano", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-03/3-CaffeAmericano_ContactShadow_Green.png?itok=Kl-bJG_e", "In the morning"),
-    product("Café au Lait", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-05/10032021_BAB_CAFE_AU_LAIT_CS-min.png?itok=JoWLDPcf", "In the afternoon"),
-    product("Capuccino", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-05/10032021_CAPPUCCINO_CS-min.png?itok=n_45xlrE", "In the evening")
-];
-
-productsArray.forEach((item)=> item.propagate());
-
+    const productsArray = [
+        productConstructor("Café Americano", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-03/3-CaffeAmericano_ContactShadow_Green.png?itok=Kl-bJG_e", "In the morning"),
+        productConstructor("Café au Lait", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-05/10032021_BAB_CAFE_AU_LAIT_CS-min.png?itok=JoWLDPcf", "In the afternoon"),
+        productConstructor("Capuccino", "https://www.starbucksathome.com/ca/sites/default/files/styles/rdp_banner_image/public/2021-05/10032021_CAPPUCCINO_CS-min.png?itok=n_45xlrE", "In the evening")
+    ];
+    
+    productsArray.forEach((item) => item.populate());    
+}
+else{
+    alert('The HTML template element is not supported.');
+}
